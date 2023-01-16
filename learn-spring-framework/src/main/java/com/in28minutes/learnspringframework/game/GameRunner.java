91b8503563f180d408a6cf92ce6f2ecb5858d65a
Shjung <wjdtngus9@gmail.com>
2023-01-16 21:29:09 +0900

    .

--- learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ learn-spring-framework/src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -5,15 +5,26 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
-	
+//필드 주입. 	runGame() 메소드, GamingConsole game 변수만 존재.
 	@Autowired
 	private GamingConsole game;
 	
+//생성자 주입.
 	
 	public GameRunner(GamingConsole game) {
+		System.out.println("Using Constructor");
 		this.game = game;
 	}
 	
+
+//setter 주입.
+//	@Autowired
+//	public void setGame(GamingConsole game) {
+//		System.out.println("Using setter");
+//		this.game = game;
+//	}
+
+
 	public void runGame() {
 		game.up();
 		game.down();
