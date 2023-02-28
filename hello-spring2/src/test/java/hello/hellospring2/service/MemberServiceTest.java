5935f9074955ae48aedeb44b3343415b7979862a
Shjung <wjdtngus9@gmail.com>
2023-02-28 23:59:20 +0900

    .

---
+++ hello-spring2/src/test/java/hello/hellospring2/service/MemberServiceTest.java
@@ -0,0 +1,19 @@
+package hello.hellospring2.service;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+	@Test
+	void join() {
+		
+	}
+	@Test
+	void findMembers() {
+		
+	}
+	
+	@Test
+	void findOne() {
+		
+	}
+}
